d53cfda964ecc7af13034d8a1f045f659bccebd7
Primus2128 <75663518+Primus2128@users.noreply.github.com>
2024-08-02 20:49:53 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "<h1> Hello, I'm Vibhanshu!! </h1>"
+def home():
+    return "<h1> Hello, I'm Vibhanshu !! </h1>"
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True, port = 3000)
